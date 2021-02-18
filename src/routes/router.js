11506d7f8b518caa5fc2e42b53c7fd24619160fc
Bit <minikraft1212@gmail.com>
2021-02-18 09:41:40 +0300

    убрал ненужное

--- src/routes/router.js
+++ src/routes/router.js
@@ -8,16 +8,7 @@ Vue.use(VueRouter);
 const router = new VueRouter({
   routes, // short for routes: routes
   mode: 'history',
-  linkActiveClass: 'active',
-  scrollBehavior: (to, from ,savedPosition) => {
-    if (savedPosition) {
-      return savedPosition;
-    }
-    if (to.hash) {
-      return { selector: to.hash };
-    }
-    return { x: 0, y: 0 };
-  }
+  linkActiveClass: 'active'
 });
 
 export default router;
